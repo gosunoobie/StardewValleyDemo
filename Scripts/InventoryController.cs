60bfd3ffb1952c5c81e1039b01a29f0c94010531
unknown <dongchholok@gmail.com>
2023-03-23 22:35:48 +0545

    controller

---
+++ Scripts/InventoryController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+[SerializeField] GameObject panel;
+
+private void Update() {
+    if(Input.GetKeyDown(KeyCode.I)){
+        panel.SetActive(!panel.activeInHierarchy);
+    }
+}
+}
