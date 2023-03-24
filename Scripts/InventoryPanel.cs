d41f911d4334190557fbfa30c79cd5d14d6eab5f
unknown <dongchholok@gmail.com>
2023-03-24 17:49:34 +0545

    toolbar started

--- Scripts/InventoryPanel.cs
+++ Scripts/InventoryPanel.cs
@@ -17,13 +17,13 @@ private void OnEnable() {
 }
 
 private void SetIndex(){
-  for(int i =0; i < inventory.slots.Count;i++){
+  for(int i =0; i < inventory.slots.Count && i < buttons.Count;i++){
 buttons[i].SetIndex(i);
   }
 }
 
 private void Show(){
-for(int i = 0;i<inventory.slots.Count; i++){
+for(int i = 0;i<inventory.slots.Count && i< buttons.Count; i++){
   if(inventory.slots[i].item == null){
     buttons[i].Clean();
   }
--- Scripts/Item.cs
+++ Scripts/Item.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 [CreateAssetMenu(menuName="Data/Item")]
 public class Item : ScriptableObject
 {
